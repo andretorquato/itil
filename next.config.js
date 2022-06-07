6c2bd3ea98e0a213ed9c2d34be4dab721284bef3
Bruno Sales <bruno.particular25@alu.ufc.br>
2022-06-07 09:50:02 -0300

    module

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [20, 32, 48, 64, 96, 128, 256, 384],
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
 import { HomeComponent } from "../src/components/Home/HomeComponent";
+import { ThemeComponent } from "../src/components/themes/Themes";
 
-const Home: NextPage = () => {
-  return <HomeComponent />;
+ const Home: NextPage = () => {
+  return (
+    <>
+      {/* <HomeComponent />; */}
+      <ThemeComponent />
+    </>
+  );
 };
 
 export default Home;
